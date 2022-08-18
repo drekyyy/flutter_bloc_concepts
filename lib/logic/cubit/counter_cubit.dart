e4697555e3d76ef7f43dac355a5ed285282219ec
Dariusz Karp <heniulu10@gmail.com>
2022-08-18 18:57:45 +0200

    debbinging cubit using onChange function to print current and new state

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -29,6 +29,21 @@ class CounterCubit extends Cubit<CounterState> with HydratedMixin {
   @override
   // called for every state emitted by CounterCubit
   Map<String, dynamic>? toJson(CounterState state) {
+    //addError(Exception('Couldnt write to storage!'), StackTrace.current);
     return state.toMap();
   }
+
+  @override
+  void onChange(Change<CounterState> change) {
+    // print(
+    //     'current state: ${change.currentState.counterValue.toString()},  next state: ${change.nextState.counterValue.toString()}');
+    print(change);
+    super.onChange(change);
+  }
+
+  // @override
+  // void onError(Object error, StackTrace stackTrace) {
+  //   print('$error, $stackTrace');
+  //   super.onError(error, stackTrace);
+  // }
 }
