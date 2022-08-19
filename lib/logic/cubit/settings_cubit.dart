d10835adb79860f46400941a754ae1d3353ead9f
Dariusz Karp <heniulu10@gmail.com>
2022-08-19 22:13:45 +0200

    implement storing SettingsState to local storage

--- lib/logic/cubit/settings_cubit.dart
+++ lib/logic/cubit/settings_cubit.dart
@@ -1,9 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'dart:convert';
+
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'settings_state.dart';
 
-class SettingsCubit extends Cubit<SettingsState> {
+class SettingsCubit extends Cubit<SettingsState> with HydratedMixin {
   SettingsCubit()
       : super(
             SettingsState(appNotifications: false, emailNotifications: false));
@@ -14,5 +17,16 @@ class SettingsCubit extends Cubit<SettingsState> {
 
   void toggleEmailNotifications(bool newValue) {
     emit(state.copyWith(emailNotifications: newValue));
+  }
+
+  SettingsState? fromJson(Map<String, dynamic> json) {
+    return SettingsState.fromMap(json);
+  }
+
+  @override
+  // called for every state emitted by CounterCubit
+  Map<String, dynamic>? toJson(SettingsState state) {
+    //addError(Exception('Couldnt write to storage!'), StackTrace.current);
+    return state.toMap();
   }
 }
--- lib/logic/cubit/settings_state.dart
+++ lib/logic/cubit/settings_state.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
 part of 'settings_cubit.dart';
 
 class SettingsState extends Equatable {
@@ -27,4 +28,23 @@ class SettingsState extends Equatable {
 
   @override
   bool get stringify => true;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'appNotifications': appNotifications,
+      'emailNotifications': emailNotifications,
+    };
+  }
+
+  factory SettingsState.fromMap(Map<String, dynamic> map) {
+    return SettingsState(
+      appNotifications: map['appNotifications'] as bool,
+      emailNotifications: map['emailNotifications'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SettingsState.fromJson(String source) =>
+      SettingsState.fromMap(json.decode(source) as Map<String, dynamic>);
 }
